9fdad52c1eed6e9f24aa276058419a0dcc92c806
André <andrerrr@outlook.de>
2019-07-24 15:56:09 +0200

    Bilder

---
+++ HalloImages/HalloImages/HalloImages/EmbeddedImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HalloImages
+{
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string Id { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(Id))
+                return null;
+
+            return ImageSource.FromResource(Id);
+        }
+    }
+}
