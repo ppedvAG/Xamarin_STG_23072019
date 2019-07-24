dd311d81e4fb8570254e2021f7514705fb44064f
André <andrerrr@outlook.de>
2019-07-24 15:07:33 +0200

    Styles mit style

---
+++ HalloStyle/HalloStyle/HalloStyle/EmailValidationTriggerAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HalloStyle
+{
+    class EmailValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (string.IsNullOrWhiteSpace(sender.Text) || !sender.Text.Contains("@"))
+                sender.BackgroundColor = Color.DeepPink;
+            else
+                sender.BackgroundColor = Color.Default;
+        }
+    }
+}
