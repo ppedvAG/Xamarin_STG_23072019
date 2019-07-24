897e81aa43b9cba52dca115d9747d43d7b5fdebe
André <andrerrr@outlook.de>
2019-07-24 16:42:20 +0200

    Binding

---
+++ HalloBinding/HalloBinding/HalloBinding/ToggleToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace HalloBinding
+{
+    class ToggleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Color.HotPink;
+
+            return Color.Azure;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
