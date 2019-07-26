a71e3bbbd483cb1434f92eca63c59897301a9877
André <andrerrr@outlook.de>
2019-07-26 10:06:48 +0200

    Hallo Hotel: Nav geht

---
+++ HalloHotel/HalloHotel/HalloHotel/Model/Buchung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HalloHotel.Model
+{
+    public class Buchung
+    {
+        public int Id { get; set; }
+        public DateTime Datum { get; set; }
+        public int Naechte { get; set; }
+        public string Gast { get; set; }
+        public string BNummer { get; set; }
+        public Hotel Hotel { get; set; }
+        public decimal GesamtPreis { get; set; }
+    }
+}
---
+++ HalloHotel/HalloHotel/HalloHotel/Model/Hotel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace HalloHotel.Model
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal PreisProNacht { get; set; }
+        public string Ort { get; set; }
+        public ICollection<Buchung> Buchungen { get; set; } = new HashSet<Buchung>();
+    }
+}
